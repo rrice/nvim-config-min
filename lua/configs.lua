c967e326934273b9d68b2c55e407a0dc2899cc3e
Ralph Allan Rice <ralph.rice@gmail.com>
2026-02-24 15:45:26 -0500

    Added Snacks.nvim

--- lua/configs.lua
+++ lua/configs.lua
@@ -8,15 +8,12 @@ local opt = vim.opt
 -- Mouse mode
 opt.mouse = "a"
 
--- Column decoration
-opt.signcolumn = "no" -- Show the sign column?
-
 -- Tab stops
 opt.autoindent = true -- Enabled auto indentation.
 opt.expandtab = false -- Use tabs instead of spaces by default, use autocmds for lang-specific
-opt.tabstop = 4	-- Number of spaces for a tab.
+opt.tabstop = 2	-- Number of spaces for a tab.
 opt.softtabstop = 4 -- Number of spaces for a tab when editing.
-opt.shiftwidth = 4 -- Number of spaces for autoindent.
+opt.shiftwidth = 2 -- Number of spaces for autoindent.
 opt.shiftround = true -- Round indent to multiples of shiftwidth.
 
 -- Character displays
@@ -26,7 +23,8 @@ opt.list = true -- Show whitespace chars
 -- Line behaviors
 opt.wrap = false -- Disable line wrapping
 opt.cursorline = true -- Highlight the current line
-opt.scrolloff = 10 -- Keep 10 lines above and below the cursor
+opt.scrolloff = 4 -- Number of lines of context
+opt.sidescrolloff = 10 -- Number of columns of context
 opt.breakindent = true -- Wrapped lines will continue to be indented
 
 -- Line number column
@@ -39,8 +37,20 @@ opt.numberwidth = 2 -- Width of the line number column
 opt.termguicolors = true -- Enable true colors
 opt.guicursor = "i:block" -- Use the block cursor in insert mode
 opt.updatetime = 250 -- Swap file update time in ms
-opt.timeoutlen = 300 -- Time to wait for mapped sequence to complete in ms
+opt.timeoutlen = vim.g.vscode and 1000 or 300 -- Time to wait for mapped sequence to complete in ms
 opt.signcolumn = "auto:4" -- Sign column display
+opt.showmode = false -- Don't show mode because we have a status line.
+opt.virtualedit = "block" -- Allow the cursor to move where there is no text.
+opt.winminwidth = 5 -- Minimum window width
+
+-- Popup behaviors 
+opt.pumblend = 10 -- Popup blend default.
+opt.pumheight = 10 -- Popup height default.
+
+-- Folding
+opt.foldlevel = 99
+opt.foldmethod = "indent"
+opt.foldtext = ""
 
 -- Paren matching
 opt.showmatch = true -- Enable matching paren
@@ -53,16 +63,22 @@ opt.smartcase = true -- Smart casing enabled
 -- Undo behaviors
 opt.undodir = os.getenv('HOME') .. '/.vim/undodir' -- Directory for undo files
 opt.undofile = true -- Enable persistent undo.
-opt.completeopt = { "menuone", "popup", "noinsert" } -- Options for completion menu
+opt.undolevels = 10000
+
+-- Completion behaviors
+opt.completeopt = { "menu", "menuone", "popup", "noinsert", "noselect" } -- Options for completion menu
 opt.winborder = "rounded" -- Use rounded borders for windows
 opt.hlsearch = false -- Disable highlighting of search results
-opt.clipboard = "unnamedplus" -- Clipboard settings
+
+-- System behaviors
+opt.autowrite = true -- Enable autowrite
+opt.clipboard = vim.env.SSH_CONNECTION and "" or "unnamedplus" -- Sync with system clipboard
 
 -- Window split behaviors
 opt.splitright = true -- split to right first on horizontal split
 opt.splitbelow = true -- split below first on vertical split
+opt.splitkeep = "screen"
 opt.inccommand = "nosplit" -- Shows the effects of a command incrementally in the buffer
-
 
 -- Plugin behaviors
 vim.cmd.filetype("plugin indent on") -- Enable filetype detection, plugins, and indentation
--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,8 +1,32 @@
 vim.pack.add({
-	{ src = "https://github.com/folke/which-key.nvim" }
+	{ src = "https://github.com/folke/which-key.nvim" },
+	{ src = "https://github.com/folke/snacks.nvim" }
 })
 
-vim.g.mapleader = ' '
+local snacks = require("snacks")
+
+snacks.setup({
+	bigfile = { enabled = true },
+	dashboard = { enabled = true },
+	explorer = { enabled = true },
+	indent = { enabled = true },
+	input = { enabled = true },
+	notifier = {
+	enabled = true,
+			timeout = 3000,
+		},
+		picker = { enabled = true },
+		quickfile = { enabled = true },
+		scope = { enabled = true },
+		scroll = { enabled = true },
+		statuscolumn = { enabled = true },
+		words = { enabled = true },
+		styles = {
+			notification = {
+				-- wo = { wrap = true } -- Wrap notifications
+			}
+		}	
+})
 
 local whichkey = require("which-key")
 
@@ -10,13 +34,20 @@ whichkey.setup({
 	preset = "helix",
 	-- Setup a popup window
 	show_warnings = false,
-	window = {
+	win = {
 		border = "rounded",
-		position = "bottom", 
-		margin = { 1, 2, 1, 2 },
+		title = true,
+		title_pos = "center",
 		padding = { 2, 2, 2, 2 },
-		winblend = 10,
 		style = "minimal",
+		wo = {
+			winblend = 10, -- A value between 0 (opaque) - 100 (transparent)
+		},
+	},
+	layout = {
+		height = { min = 1, max = 10 }, -- Dynamically adjust the height.
+		width = { min = 1, max = 50 }, -- Dynamically adjust the width.
+		spacing = 3, -- Spacing between columns.
 	},
 	spec = {
 		mode = { "n", "x" },
@@ -27,9 +58,10 @@ whichkey.setup({
 		{ "<leader>f", group = "file/find" },
 		{ "<leader>g", group = "git" },
 		{ "<leader>gh", group = "hunks" },
+		{ "<leader>q", group = "quit/session" },
 		{ "<leader>s", group = "search" },
 		{ "<leader>u", group = "ui" },
-		{ "<leader>q", group = "diagnostics/quickfix" },
+		{ "<leader>x", group = "diagnostics/quickfix" },
 		{ "[", group = "prev" },
 		{ "]", group = "next" },
 		{ "g", group = "goto" },
@@ -52,12 +84,13 @@ whichkey.setup({
 		},
 		-- better descriptions
 		{ "gx", desc = "Open with system app" },
-	},
-	layout = {
-		height = { min = 1, max = 10 }, -- Dynamically adjust the height.
-		width = { min = 1, max = 50 }, -- Dynamically adjust the width.
-		spacing = 3, -- Spacing between columns.
-	},
+				},
+
+	triggers = {
+		{ "<leader>", mode = { "n", "v" }},
+		{ "<auto>", mode = "nixsotc" },
+		{ "a", mode = { "n", "v" }},
+	}
 })
 
 local keymap = vim.keymap.set
@@ -68,29 +101,79 @@ vim.opt.hlsearch = true
 
 -- Diagnostic keymap bindings.
 vim.diagnostic.config({
-  virtual_text = false,
-  signs = true,
-  float = {
-    border = "single",
-    format = function(d)
-      return string.format("%s (%s) [%s]",
-        d.message,
-        d.source,
-        d.code or d.user_data.lsp.code)
-    end,
-  },
+	virtual_text = false,
+	signs = true,
+	float = {
+		border = "single",
+		format = function(d)
+			return string.format("%s (%s) [%s]",
+				d.message,
+				d.source,
+				d.code or d.user_data.lsp.code)
+		end,
+	},
 })
 
 keymap("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
+-- Diagnostics
+local diagnostic_goto = function(next, severity)
+  return function()
+    vim.diagnostic.jump({
+      count = (next and 1 or -1) * vim.v.count1,
+      severity = severity and vim.diagnostic.severity[severity] or nil,
+      float = true,
+    })
+  end
+end
 
-keymap("n", "[d", vim.diagnostic.get_prev, { desc = "Go to previous [D]iagnostic message" })
-keymap("n", "]d", vim.diagnostic.get_next, { desc = "Go to next [D]iagnostic message" })
-keymap("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
-keymap("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uick fix list" })
+keymap("n", "<leader>cd", vim.diagnostic.open_float, { desc = "Line Diagnostics" })
+keymap("n", "]d", diagnostic_goto(true), { desc = "Next Diagnostic" })
+keymap("n", "[d", diagnostic_goto(false), { desc = "Prev Diagnostic" })
+keymap("n", "]e", diagnostic_goto(true, "ERROR"), { desc = "Next Error" })
+keymap("n", "[e", diagnostic_goto(false, "ERROR"), { desc = "Prev Error" })
+keymap("n", "]w", diagnostic_goto(true, "WARN"), { desc = "Next Warning" })
+keymap("n", "[w", diagnostic_goto(false, "WARN"), { desc = "Prev Warning" })
 
 -- Simplified window navigation.
-keymap("n", "<C-D-Left>", "<C-w><C-h>", { desc = "Move focus to left window" })
-keymap("n", "<C-D-Right>", "<C-w><C-l>", { desc = "Move focus to right window" })
-keymap("n", "<C-D-Down>", "<C-w><C-j>", { desc = "Move focus to lower window" })
-keymap("n", "<C-D-Up>", "<C-w><C-k>", { desc = "Move focus to upper window" })
+keymap({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
+keymap({ "n", "x" }, "<Down>", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
+keymap({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
+keymap({ "n", "x" }, "<Up>", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
+
+-- Move to window using the <ctrl> hjkl keys
+keymap("n", "<C-h>", "<C-w>h", { desc = "Go to Left Window", remap = true })
+keymap("n", "<C-j>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
+keymap("n", "<C-k>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
+keymap("n", "<C-l>", "<C-w>l", { desc = "Go to Right Window", remap = true })
+
+-- Resize window using <ctrl> arrow keys
+keymap("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
+keymap("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
+keymap("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
+keymap("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
+
+-- Move Lines
+keymap("n", "<A-j>", "<cmd>execute 'move .+' . v:count1<cr>==", { desc = "Move Down" })
+keymap("n", "<A-k>", "<cmd>execute 'move .-' . (v:count1 + 1)<cr>==", { desc = "Move Up" })
+keymap("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
+keymap("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
+keymap("v", "<A-j>", ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv", { desc = "Move Down" })
+keymap("v", "<A-k>", ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", { desc = "Move Up" })
+
+-- Buffer Management
+keymap("n", "<S-h>", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
+keymap("n", "<S-l>", "<cmd>bnext<cr>", { desc = "Next Buffer" })
+keymap("n", "[b", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
+keymap("n", "]b", "<cmd>bnext<cr>", { desc = "Next Buffer" })
+keymap("n", "<leader>bb", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
+keymap("n", "<leader>`", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
+keymap("n", "<leader>bd", function()
+	Snacks.bufdelete()
+end, { desc = "Delete Buffer" })
+keymap("n", "<leader>bo", function()
+	Snacks.bufdelete.other()
+end, { desc = "Delete Other Buffers" })
+keymap("n", "<leader>bD", "<cmd>:bd<cr>", { desc = "Delete Buffer and Window" })
+
+
